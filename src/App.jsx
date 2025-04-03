e77e0d3fb8bd4af04f66ae9145600ab7fd6a3249
tk <wangtk957@gmail.com>
2025-04-03 16:32:05 +1030

    test main env

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import AppRoutes from './routes'
 function App() {
   const apiUrl = import.meta.env.VITE_APP_API_URL // 使用 Vite 的环境变量
 
-  console.log('VITE_APP_API_URL:', apiUrl)
+  console.log('VITE_APP_API_URL: ', apiUrl)
   return (
     <Router>
       <AppRoutes />
